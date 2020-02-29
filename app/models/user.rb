23a9e7df797941b2a84b4cc2a0631959698643f3
tamanegi <tamanegi@example.com>
2020-02-29 12:29:31 +0900

    ユーザーモデルのテストを更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 45 }
-    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+                      length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX }
     validates :favorite_genre, length: { maximum: 45 }
+    validates :profile, length: { maximum: 255 }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true, length: { minimum: 6 }
 end
