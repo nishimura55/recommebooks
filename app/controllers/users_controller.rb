b1d490a76fa70562ed6d89f6754f34795ac60678
tamanegi <tamanegi@example.com>
2020-03-01 11:48:50 +0900

    JavaScriptの有効化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "ユーザー登録が完了しました。レコメブックスへようこそ！"
+            redirect_to @user
+        else
+            render 'new'
+        end
     end
+
+    private
+
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
+
 end
