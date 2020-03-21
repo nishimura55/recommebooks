6e682e81e37f50eeeff221e41f2cf3f349a78b0e
tamanegi <tamanegi@example.com>
2020-03-21 22:34:18 +0900

    本一覧画面を実装

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -5,6 +5,44 @@ RSpec.describe '本のシステムテスト', type: :system do
     let(:other_user) { create(:user) }
     let(:admin_user) { create(:user, :admin_user) }
 
+    describe '本一覧のテスト' do
+
+        before do
+            create_list(:book, 31)
+            visit books_path
+        end
+
+        context '一般ユーザーの場合' do
+            it '本一覧ページが表示される' do
+                expect(page).to have_title '本一覧 | Recommebooks'
+                expect(page).to have_selector '.pagination'
+                Book.paginate(page: 1).each do |book|
+                    expect(page).to have_link book.title, href: book_path(book)
+                    expect(page).not_to have_link '削除', href: book_path(book)
+                end
+            end
+        end
+
+        context '管理ユーザーの場合' do
+            it '削除リンクつきの本一覧ページが表示され本削除が可能' do
+                log_in_as(admin_user)
+                visit books_path
+                expect(page).to have_title '本一覧 | Recommebooks'
+                expect(page).to have_selector '.pagination'
+                Book.paginate(page: 1).each do |book|
+                    expect(page).to have_link book.title, href: book_path(book)
+                    expect(page).to have_link '削除', href: book_path(book)
+                end
+                expect do
+                  page.all('#delete')[0].click
+                  page.accept_confirm
+                  expect(page).to have_content '削除しました'
+                end.to change {Book.count}.by(-1)
+            end
+        end
+
+    end
+
     describe '本の投稿のテスト' do
 
         before do
@@ -23,7 +61,7 @@ RSpec.describe '本のシステムテスト', type: :system do
                 expect do
                     fill_in 'book_search', with: '山'
                     click_on '本の題名を検索'
-                    page.all('#thmb-book-btn')[1].click
+                    page.all('#thmb-book-btn')[0].click
                     fill_in '投稿者の感想', with: 'そうとう面白かった！'
                     click_on '投稿する'
                 end.to change {Book.count}.by(+1)
