b4cb73d750398779e4bc1a3e01035f40c08fa1e1
nishimura55 <naoki.hnmr55@gmail.com>
2020-05-12 00:23:11 +0900

    carrierwaveの設定ファイルを変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ unless Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
       config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: '<%= ENV['S3_ACCESS_KEY'] %>',
-        aws_secret_access_key: '<%= ENV['S3_SECRET_ACCESS_KEY'] %>',
+        aws_access_key_id: ENV['S3_ACCESS_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
         region: 'ap-northeast-1'
       }
   
       config.fog_directory  = 'recommebooks-s3'
       config.cache_storage = :fog
     end
-  end
+end
