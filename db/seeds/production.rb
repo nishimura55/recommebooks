041ac774abc3014154fe82254ceb29834c8e02a5
nishimura55 <naoki.hnmr55@gmail.com>
2020-05-12 19:26:07 +0900

    seedsを追加

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -15,3 +15,53 @@ User.create!(
    password_confirmation: ENV['ADMIN_PASSWORD'],
    admin: true
 )
+
+Genre.create!(
+    division: 1,
+    name: '文学・小説'
+)
+Genre.create!(
+    division: 1,
+    name: '現代小説'
+)
+Genre.create!(
+    division: 1,
+    name: 'ミステリー'
+)
+Genre.create!(
+    division: 1,
+    name: '歴史小説'
+)
+Genre.create!(
+    division: 1,
+    name: '映画原作'
+)
+Genre.create!(
+    division: 1,
+    name: 'ノンフィクション'
+)
+Genre.create!(
+    division: 1,
+    name: 'ファンタジー'
+)
+Genre.create!(
+    division: 1,
+    name: 'SF'
+)
+Genre.create!(
+    division: 1,
+    name: 'ホラー'
+)
+Genre.create!(
+    division: 1,
+    name: 'ライトノベル'
+)
+Genre.create!(
+    division: 1,
+    name: '海外文学'
+)
+Genre.create!(
+    division: 1,
+    name: 'エッセー'
+)
+
