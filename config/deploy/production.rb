76a07aabfdd330b20eb8005452b741eda966cae1
nishimura55 <naoki.hnmr55@gmail.com>
2020-05-25 12:05:05 +0900

    CircleCI上でのCapistranoの設定1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,16 @@
-server '52.198.23.227',
+server ENV['EC2_IP_ADRESS'],
+    port: 22,
     user: 'naoki',
     roles: %w{app db web},
+    primary: true,
     ssh_options: {
         user: 'naoki',
-        keys: '~/.ssh/aws/recommebooks.pem',
+        keys: ["#{ENV['EC2_SSH_KEY_PASS']}"],
         forward_agent: true
     }
+
+
+
+    # '52.198.23.227'
+
+    # '~/.ssh/aws/recommebooks.pem'
