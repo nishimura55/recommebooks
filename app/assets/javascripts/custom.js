defc770437d48d5a94ad2d3cd196823a2b7bc35a
nishimura55 <naoki.hnmr55@gmail.com>
2020-05-28 16:34:02 +0900

    レコメンドフォームなどの調整

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -20,4 +20,39 @@ window.onload = function() {
         $target2.classList.toggle('is-hidden')
       })
     }
+    var $target3 = document.querySelector('.target3')
+    var $whatrep3 = document.querySelector('.whatrep3')
+    if($target3){
+      $whatrep3.addEventListener('click', function() {
+        $target3.classList.toggle('is-hidden')
+      })
+    }
+    var $target4 = document.querySelector('.target4')
+    var $whatrep4 = document.querySelector('.whatrep4')
+    if($target4){
+      $whatrep4.addEventListener('click', function() {
+        $target4.classList.toggle('is-hidden')
+      })
+    }
+    var $target5 = document.querySelector('.target5')
+    var $whatrep5 = document.querySelector('.whatrep5')
+    if($target5){
+      $whatrep5.addEventListener('click', function() {
+        $target5.classList.toggle('is-hidden')
+      })
+    }
+    var $target6 = document.querySelector('.target6')
+    var $whatrep6 = document.querySelector('.whatrep6')
+    if($target6){
+      $whatrep6.addEventListener('click', function() {
+        $target6.classList.toggle('is-hidden')
+      })
+    }
+    var $target7 = document.querySelector('.target7')
+    var $whatrep7 = document.querySelector('.whatrep7')
+    if($target7){
+      $whatrep7.addEventListener('click', function() {
+        $target7.classList.toggle('is-hidden')
+      })
+    }
 }
